553d7e7198af053da8c53c97f842b91d74ce4e1f
nfsulthan <sulthansiahaan@gmail.com>
2023-11-06 23:06:51 +0700

    Pertemuan 6

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Nur Fatahilah Sulthan
+        Saya tinggal di daerah Kubu Raya
+        Olahraga yang saya sukai adalah
+        Berenang";
+    }
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section> 
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4> <ul type="disc"> 
+        <li>Nama Depan : Nur Fatahilah</li> 
+        <li>Nama Belakang : Sulthan</li> 
+    </ul> 
+    <br> 
+    <h4>Alamat</h4> 
+    <ul type="none"> 
+        <li> Jalan Raya Desa Kapur ,Komplek Green Kapur Village No B7</li> 
+    </ul> 
+    
+    <h4>Tempat Lahir</h4> 
+    <ul type="none"> 
+        <li>Jakarta</li> 
+    </ul> 
+    
+    <h4>Olah Raga Favorit</h4> 
+    <ul type="square"> 
+        <li>Berenang</li> 
+        <li>Bermain Game</li> 
+    </ul> 
+</section>
